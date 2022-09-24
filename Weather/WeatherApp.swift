d9c7c5183752ea34a65c76778414d4f9a2bb5f17
Yanish-bjn <yanish.bhujun@gmail.com>
2022-09-24 13:12:52 +0200

    Initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Yanish Bhujun on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
