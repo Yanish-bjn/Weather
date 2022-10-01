8663c43c1e39a71bcef2d0996d77da51ed3c0871
Yanish-bjn <yanish.bhujun@gmail.com>
2022-10-01 16:34:57 +0200

    :white_check_mark:
    Création d'une fonction dans le but d'arrondir les chiffres obtenu par l'API

---
+++ Weather/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by Yanish Bhujun on 01/10/2022.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        
+        return String(format: "%.0f", self)
+    }
+}
