3d3d67253cea71d68e314543cfbd78a0107718c2
Yanish-bjn <yanish.bhujun@gmail.com>
2022-10-01 18:58:41 +0200

    :hammer:
    Ajout de nouvelles interfaces

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -1,51 +1,101 @@
 //
-//  ContentView.swift
+//  WeatherView.swift
 //  Weather
 //
-//  Created by Yanish Bhujun on 24/09/2022.
+//  Created by Yanish Bhujun on 01/10/2022.
 //
 
 import SwiftUI
 
-
 struct WeatherView: View {
-    @StateObject var locationManager = LocationManager()
-    
-     
-    @ObservedObject var viewModel: WeatherViewModel
+    var weather: ResponseBody
     
     var body: some View {
-        VStack {
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing:20){
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) {
+                        image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
             
-            if let location = locationManager.location {
-                Text("Vos coordonées géographique sont: \(location.longitude), \(location.latitude)")
-                     } else {
-                        if locationManager.isLoading {
-                            LoadingView()
-                        } else {
-                            WelcomeView()
-                                .environmentObject(locationManager)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack{
+                        WeatherRow(logo: "thermometer.snowflake", name: "Min temp", value:(weather.main.tempMin.roundDouble() + "°"))
+                        Spacer()
+                        WeatherRow(logo:"thermometer.sun.fill", name: "Max temp ", value:(weather.main.tempMax.roundDouble() + "°"))
+                        
+                    }
+                    HStack{
+                        WeatherRow(logo: "wind", name: "Wind speed", value:(weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value:(weather.main.humidity.roundDouble() + "%"))
+
                     }
                 }
-            Text(viewModel.cityName)
-                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
                 .padding()
-            Text(viewModel.temperature)
-                .font(.system(size: 70))
-                .bold()
-            Text(viewModel.weatherIcon)
-                .font(.largeTitle)
-                .padding()
-            Text(viewModel.weatherDescription)
-        }.onAppear(perform: viewModel.refresh)
-            .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
-            .preferredColorScheme(.dark)
-    }
-}
-    
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeatherView(viewModel: WeatherViewModel(weatherService: WeatherService()))
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
